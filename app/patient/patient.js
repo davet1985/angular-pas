5c35adaaea6d42f79c774e604cbc7de233576bba
Dave Thompson <davethompson21@gmail.com>
2015-04-08 13:53:11 +0100

    Creating (POST) patient
    Also renaming templates and using a partial template for the patient
    form.

--- app/patient/patient.js
+++ app/patient/patient.js
@@ -10,14 +10,14 @@ angular.module('myApp.patient', ['ngRoute', 'ngResource', 'configService'])
     })
     .when('/patient', {
       templateUrl: 'patient/create.html',
-      controller: 'PatientListCtrl'
+      controller: 'PatientCreateCtrl'
     })
     .when('/patient/:id', {
-      templateUrl: 'patient/patient.html',
+      templateUrl: 'patient/view.html',
       controller: 'PatientViewCtrl'
     })
     .when('/patient/:id/edit', {
-      templateUrl: 'patient/patient-edit.html',
+      templateUrl: 'patient/edit.html',
       controller: 'PatientEditCtrl'
     });
 }])
@@ -42,7 +42,6 @@ angular.module('myApp.patient', ['ngRoute', 'ngResource', 'configService'])
 }])
 
 .controller('PatientViewCtrl', ['$scope', '$routeParams', '$location', 'Patient', function($scope, $routeParams, $location, Patient) {
-  $scope.editFlag = $routeParams.edit;
   Patient.get({ id: $routeParams.id }, function(data) {
     $scope.patient = data;
   });
@@ -50,11 +49,9 @@ angular.module('myApp.patient', ['ngRoute', 'ngResource', 'configService'])
   $scope.edit = function() {
     $location.path("/patient/" + $routeParams.id + "/edit");
   };
-
 }])
 
 .controller('PatientEditCtrl', ['$scope', '$routeParams', '$location', 'Patient', function($scope, $routeParams, $location, Patient) {
-
   Patient.get({ id: $routeParams.id }, function(data) {
     $scope.patient = data;
   });
@@ -68,5 +65,16 @@ angular.module('myApp.patient', ['ngRoute', 'ngResource', 'configService'])
       }
     );
   };
+}])
 
+.controller('PatientCreateCtrl', ['$scope', '$routeParams', '$location', 'Patient', function($scope, $routeParams, $location, Patient) {
+  $scope.submit = function() {
+    Patient.save(
+      {},
+      $scope.patient,
+      function() {
+        $location.path("/patients");
+      }
+    );
+  };
 }]);
