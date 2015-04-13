445c4104b313e2f6fe04458c25d24f0afc952d70
Dave Thompson <davethompson21@gmail.com>
2015-04-13 13:36:43 +0100

    Amending express server for heroku

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/app'));
-app.listen(process.env.PORT || 3000);
+
+port = process.env.PORT || 8000;
+
+app.use('/public', express.static('public'));
+app.use('/app', express.static('app'));
+
+app.use(require('prerender-node').set('prerenderToken', '5QUDyBsGmfyDFqVugAQ2'));
+
+app.get('/', function (req, res) {
+    res.sendfile('./index.html');
+});
+
+app.listen(port);
+
+console.log("Express server running at => http://localhost:" + port + "/\nCTRL + C to shutdown");
